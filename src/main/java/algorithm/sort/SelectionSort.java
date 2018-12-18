5a5c18ebb3db83ada07ef3cbf35038ae576d14dd
keshawn <fkx@live.cn>
2018-12-18 21:25:33 +0800

    SelectionSort

---
+++ src/main/java/algorithm/sort/SelectionSort.java
@@ -0,0 +1,38 @@
+package algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ * 两层循环
+ * 内部循环每次找出未排序部分的最小值
+ * 外部循环每次拿到内部循环找到的最小值设置到左边
+ */
+public class SelectionSort {
+
+    public static void sort(int[] items) {
+        if (items.length <= 1) return;
+
+        for (int i = 0; i < items.length; i++) {
+            int minIndex = i;
+            int minValue = items[i];
+            for (int j = i; j < items.length; j++) {
+                if (items[j] < minValue) {
+                    minValue = items[j];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = items[i];
+                items[i] = items[minIndex];
+                items[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {2, 3, 5, 1, 2, 4, 6};
+        sort(ints);
+        System.out.println(Arrays.toString(ints));
+    }
+}
